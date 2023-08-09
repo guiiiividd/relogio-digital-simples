e872925d5a32fa076f78b9a59e3408153c984117
guiiiividd <guiiiividd@gmail.com>
2023-08-09 16:11:17 -0300

    Aplicando funcionabilidade ao relógio

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,15 @@
-console.log("Olá mundo!")
+const hoursSpan = document.getElementById("hours-span");
+const minutesSpan = document.getElementById("minutes-span");
+const secondsSpan = document.getElementById("seconds-span");
+
+setInterval(() => {
+    const dateTime = new Date()
+    let currentHour = dateTime.getHours();
+    let currentMinute = dateTime.getMinutes();
+    let currentSecond = dateTime.getSeconds();
+
+    hoursSpan.innerText = currentHour < 10 ? `0${currentHour}` : currentHour;
+    minutesSpan.innerText = currentMinute < 10 ? `0${currentMinute}` : currentMinute;
+    secondsSpan.innerText = currentSecond < 10 ? `0${currentSecond}` : currentSecond;
+    
+}, 1000);
